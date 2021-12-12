e2aa0b98c79aa322337d6801346066143f941c90
ivancheban <ivancheban@gmail.com>
2021-12-12 17:01:45 +0200

    changed the first page to intro.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorial-basics/create-a-page">
+            to="/docs/intro">
             Start here
           </Link>
         </div>
