c762a4ff9f03bb1c33348fff4d52c57e2e54d143
Ivan Cheban <ivancheban@gmail.com>
2025-02-11 20:25:19 +0200

    Added shepherd.js for site tour

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Shepherd from 'shepherd.js';
+import 'shepherd.js/dist/css/shepherd.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -16,7 +18,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/default-docs/intro">
+            to="/docs/default-docs/intro"
+          >
             Start here
           </Link>
         </div>
@@ -26,15 +29,95 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+  const footerRef = useRef(null);
+
+  useEffect(() => {
+    const tour = new Shepherd.Tour({
+      defaultStepOptions: {
+        cancelIcon: {
+          enabled: true,
+        },
+        classes: 'shepherd-theme-dark',
+        scrollTo: { behavior: 'smooth', block: 'center' },
+      },
+    });
+
+    tour.addStep({
+      title: 'This is the site title.',
+      text: 'You can change it in docusaurus.config.js.',
+      attachTo: { element: '.hero__title', on: 'bottom' },
+      buttons: [
+        {
+          text: 'Next',
+          action: tour.next,
+        },
+      ],
+    });
+
+    tour.addStep({
+      title: 'This is the site tagline.',
+      text: 'You can change in docusaurus.config.js.',
+      attachTo: { element: '.hero__subtitle', on: 'bottom' },
+      buttons: [
+        {
+          text: 'Back',
+          action: tour.back,
+        },
+        {
+          text: 'Next',
+          action: tour.next,
+        },
+      ],
+    });
+
+    tour.addStep({
+      title: 'These are the site features',
+      text: 'You can change them in src/components/HomepageFeatures.js.',
+      attachTo: { element: '.features h3:first-child', on: 'bottom' },
+      buttons: [
+        {
+          text: 'Back',
+          action: tour.back,
+        },
+        {
+          text: 'Next',
+          action: tour.next,
+        },
+      ],
+    });
+
+    tour.addStep({
+      title: 'This is the site footer',
+      text: 'You can change the footer items in docusaurus.config.js.',
+      attachTo: { element: footerRef.current, on: 'top' },
+      buttons: [
+        {
+          text: 'Back',
+          action: tour.back,
+        },
+        {
+          text: 'End Tour',
+          action: tour.complete,
+        },
+      ],
+    });
+
+    tour.start();
+  }, []);
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
+      <footer ref={footerRef}>
+        {/* Your footer content goes here */}
+      </footer>
     </Layout>
   );
 }
